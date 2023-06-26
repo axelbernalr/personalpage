3d40a90d03e59446db4ad0bc75354a0b6c24c10c
Axel <133918721+axelbernalr@users.noreply.github.com>
2023-06-26 18:01:34 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+  function toggleMode() {
+      const body = document.body;
+      const currentMode = body.getAttribute("data-mode");
 
+      if (currentMode === "dark") {
+        body.setAttribute("data-mode", "light");
+      } else {
+        body.setAttribute("data-mode", "dark");
+      }
+    }
